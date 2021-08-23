3ff7b6d55d104755a4014e2b167deb389b2a1b0f
Julius Franklin <jjfranklin88@outlook.com>
2021-08-23 17:20:48 -0400

    sneaker data model

--- app.js
+++ app.js
@@ -2,7 +2,57 @@
 
 // initialize global state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// set event listeners
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
+const sneakers = [{
+    id: 1,
+    name: 'blue flints',
+    image: './assets/blueflints.png',
+    description: 'Jordan 13 retro flint',
+    category: 'air-jordans',
+    price: 149.99
+},
+
+{
+    id: 1,
+    name: 'Air Max 95',
+    image: './assets/airmax-removebg-preview.png',
+    description: 'Nike Air Max 95 Brown',
+    category: 'Air max',
+    price: 129.99
+},
+
+{
+    id: 3,
+    name: 'Lebron 17',
+    image: './assets/airmax-removebg-preview.png',
+    description: 'space jam lebron 17 ',
+    category: 'lebron',
+    price: 159.99
+},
+
+{
+    id: 4,
+    name: 'Yeezy 700',
+    image: './assets/yezzy-removebg-preview.png',
+    description: 'yezzy 700 v3 dark glow',
+    category: 'Yeezy Collection',
+    price: 265.00
+
+},
+
+{
+    id: 5,
+    name: 'Nike Dunks',
+    image: './assets/dunks-removebg-preview.png',
+    description: 'Dunk high 1985 acid wash',
+    category: 'Dunks',
+    price: 149.99
+}
+
+
+];
+
+console.log(sneakers);
